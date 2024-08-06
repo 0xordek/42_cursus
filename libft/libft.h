94d2b33ad229a16462cdd2870fe888a38ecd5eb2
sametozgisi <0xordek@gmail.com>
2024-08-06 23:07:39 +0300

    added

--- libft/libft.h
+++ libft/libft.h
@@ -40,7 +40,7 @@ char    *ft_strnstr(const char *haystack, const char *needle, size_t len); // ok
 char    *ft_strtrim(char const *s1, char const *set); // ok
 char    *ft_strchr(const char *str, int ch); // ok
 char    *ft_strrchr(const char *str, int ch); // ok
-void    ft_striteri(char *s, void (*f)(unsigned int, char *));
+void    ft_striteri(char *s, void (*f)(unsigned int, char *)); // ok
 char    *ft_substr(char const *s, unsigned int start, size_t len);
 char    *ft_strmapi(char const *s, char (*f)(unsigned int, char));
 char    **ft_split(char const *s, char c);
